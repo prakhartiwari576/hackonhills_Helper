f92e86cf23be46e368dea44a3788d22ccc18e588
PRAKHAR TWAIRI <prakhartiwari576@gmail.com>
2019-03-09 11:48:17 +0530

    First Commit

---
+++ app/src/main/java/org/example/android/helper/MainActivity.java
@@ -0,0 +1,25 @@
+package org.example.android.helper;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        findViewById(R.id.register).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this,RegisterActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
